759aa710b1c1d1b4c01c466007538a1b3e54f4d0
Nanounit <nanounitx@gmail.com>
2016-05-10 15:58:04 -0400

    main updates
    added some loops

--- src/Main.java
+++ src/Main.java
@@ -11,30 +11,36 @@ public class Main {
 		Machine<LaundryOrder> dry = new Dryer();
 		//end of object setup
 		
-		System.out.println("Hello what is your name?");
-		
+		System.out.println("Hello what is your name? (if you came to the wrong store type 'exit' to leave)");
 		while(sc.hasNext())
 		{
+			
 			String name = sc.next();
 			Customer p1 = new Customer(name);//(JOptionPane.showInputDialog("what is your name?"));
 			if(name.equals("exit")){break;}
 			
 		
-		System.out.println("type 1 for colored clothes type 0 for white clothing!");
+		System.out.println("type 1 for colored clothes type 0 for white clothing! (To end enter -1)");
 		int color = sc.nextInt();
+		if(color == -1){break;}
+		if(color != 1 && color !=0){System.out.println("Invalid value you entered " + color +" please try again");break;}
 		if(color == 1){
-		System.out.println("How many colored shirts do you have today? (To end type 'exit' at anytime)");
+		System.out.println("How many colored shirts do you have today? (To end type '-1' at anytime)");
 		int shirt = sc.nextInt();
-		System.out.println("How many colored pants do you have today? (To end type 'exit' at anytime)");
+		if(shirt == -1){break;}
+		System.out.println("How many colored pants do you have today? (To end type '-1' at anytime)");
 		int pant = sc.nextInt();
-		System.out.println("How many colored socks do you have today? (To end type 'exit' at anytime)");
+		if(pant == -1){break;}
+		System.out.println("How many colored socks do you have today? (To end type '-1' at anytime)");
 		int sock = sc.nextInt();
+		if(sock == -1){break;}
 		p1.createOrder(shirt, pant, sock, true, p1.name);
 		work.setOrder(p1.Corder);
 		//System.out.println(work.order.toString());
 		System.out.println("The order amount is: " + work.calculateCost()+ "$");
-		System.out.println("How much money do you have to pay? (To end type 'exit' at anytime)");
+		System.out.println("How much money do you have to pay? (To end type '-1' at anytime)");
 		double cash = sc.nextDouble(); // holds the amount the customer has to pay
+		if(cash == -1){break;}
 		work.takeOrder(p1.Corder, cash); // sets the order up with the amount of money the customer has to pay
 		if(work.getOrderStatus())
 			{
@@ -48,20 +54,24 @@ public class Main {
 			
 			}
 		else{System.exit(1);}}
-		else if(color == 0)
+		if(color == 0)
 		{
-		System.out.println("How many non-colored shirts do you have today? (To end type 'exit' at anytime)");
+		System.out.println("How many non-colored shirts do you have today? (To end type '-1' at anytime)");
 		int shirt = sc.nextInt();
-		System.out.println("How many non-colored pants do you have today? (To end type 'exit' at anytime)");
+		if(shirt == -1){break;}
+		System.out.println("How many non-colored pants do you have today? (To end type '-1' at anytime)");
 		 int pant = sc.nextInt();
-		System.out.println("How many non-colored socks do you have today? (To end type 'exit' at anytime)");
+		 if(pant == -1){break;}
+		System.out.println("How many non-colored socks do you have today? (To end type '-1' at anytime)");
 		int sock = sc.nextInt();
+		if(sock == -1){break;}
 		p1.createOrder(shirt, pant, sock, true, p1.name);
 		work.setOrder(p1.order);
 		//System.out.println(work.order.toString());
 		System.out.println("The order amount is: " + work.calculateCost()+ "$");
-		System.out.println("How much money do you have to pay?");
+		System.out.println("How much money do you have to pay? (To end type '-1)");
 		double cash = sc.nextDouble(); // holds the amount the customer has to pay
+		if(cash == -1){break;}
 		work.takeOrder(p1.order, cash); // sets the order up with the amount of money the customer has to pay
 		if(work.getOrderStatus())
 			{
@@ -76,7 +86,9 @@ public class Main {
 			}
 		
 		}
-		
+		System.out.println("Do you have another order of clothes to make? Y/N" );
+		String end = sc.next();
+		if(end == "n" || end == "N"){break;}
 		}
 		
 		
